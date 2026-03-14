738b83be5b44c4228e5c5802733dbd2d81cf03f9
Rasmus Skov <148750120+skovbov@users.noreply.github.com>
2026-03-14 01:57:11 +0100

    test(arch): implemented architecture tests

---
+++ tests/TraceApp.ArchitectureTests/BaseTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using TraceApp.Api.Endpoints.Metrics;
+using TraceApp.Application.Metrics.Commands.RecordMetric;
+using TraceApp.Domain.Entities;
+using TraceApp.Infrastructure.Persistence;
+
+namespace TraceApp.ArchitectureTests;
+
+public abstract class BaseTest
+{
+    protected static readonly Assembly DomainAssembly = typeof(ApiRequestMetric).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(RecordMetricCommandHandler).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(TraceLensDbContext).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(MapEndpoints).Assembly;
+}
